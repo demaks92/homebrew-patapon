a6e8e00059210370e2246f6f57f6300e449672ec
Denis Maksimov <wc3orb@gmail.com>
2026-07-17 22:46:26 +0300

    feat(peekx): added cask for PeekX app

--- Formula/font-patcher.rb
+++ Formula/font-patcher.rb
@@ -4,7 +4,7 @@
 # Installs the macOS patched font installer script.
 class FontPatcher < Formula
   desc "Patch fonts with Nerd Fonts glyphs on macOS"
-  homepage "https://github.com/elky92/homebrew-private"
+  homepage "https://github.com/demaks92/homebrew-patapon"
   url "file://#{File.expand_path("../scripts/font-patcher", __dir__)}"
   version "0.1.0"
   sha256 "79f5083b6fe71b355e9cf2b07a03c35d92a926e04505499c119b55a69fc7add3"
